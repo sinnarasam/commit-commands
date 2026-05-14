e2794d5e7450bf1bdac69da79a77c0778a9905d3
Sinnara <sinnara0191@gmail.com>
2026-05-14 17:15:04 +0000

    Add user input for calculation

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ def add(a, b):
 
 
 if __name__ == "__main__":
-    result = add(5, 3)
-    print(f"5 + 3 = {result}")
+    a = int(input("첫 번째 숫자: "))
+    b = int(input("두 번째 숫자: "))
+    result = add(a, b)
+    print(f"{a} - {b} = {result}")
